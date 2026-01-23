9c7e569968c20d65daddb9deca0e46f659690bcd
vanitha setty <settyvanitha03@gmail.com>
2026-01-23 10:49:29 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+/*var add=()=>{
+    console.log(10+20);
+}
+add();*/
+//spread operator
+/*var arr1=[10,20,30];
+var arr2=[...arr1,40,50,60];
+var arr3=[...arr2,76,87,65];
+console.log(arr3);*/
+
+//destructuring operator
+/*var[m1,m2,m3,m4,m5]=[[34,89],99,90,76,87]
+console.log("m1 =" + m1);
+console.log("m2 =" + m2);
+console.log("m3 =" + m3);
+console.log("m4 =" + m4);
+console.log("m5 =" + m5);*/
+var{name,age,dept}={
+    name:"vanitha setty",
+    age:20,
+    dept:["cybersecurity"]
+    marks:
+}
+console.log(name);
+console.log(age);
+console.log(dept);
